5efa0acbcbf947803bea64d10a0c111d8b873b40
Luis Garcia <bigluisbad@gmail.com>
2022-06-09 23:40:17 -0400

    disenando pagina list

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,14 +1,42 @@
 import React from 'react'
+import iconLocation from '../assets/images/location.svg'
+import iconSearch from '../assets/images/search.svg'
 
 export const List = () => {
   return (
-    <div>
-      <h1>List</h1>
-      <div className='box'>2</div>
-      <div className='box'>2</div>
-      <div className='box'>2</div>
-      <div className='box'>2</div>
-      <div className='box'>2</div>
+    <div className='container'>
+      <div className='page'>
+        <div className='search'>
+          <form className='search-form'>
+            <button className='btn-form' type='submit'
+            >
+              <img 
+                src={iconSearch} 
+                alt="icon location" 
+                className='icon-s'
+          />
+            </button>
+            <input 
+              type="text"
+              placeholder='Buscar'
+              autoComplete='off'
+              className='form-input'
+           />
+          </form>
+          <img 
+            src={iconLocation} 
+            alt="icon location" 
+            className='icon-s'
+          />
+        </div>
+        <div className='search-cards'>
+          <div className='card-s'>card</div>
+          <div className='card-s'>card</div>
+          <div className='card-s'>card</div>
+          <div className='card-s'>card</div>
+          <div className='card-s'>card</div>
+        </div>
+      </div>
     </div>
   )
 }
