61920189022678d40965f685453b011f2bc809b9
Luis Garcia <bigluisbad@gmail.com>
2022-06-14 16:58:15 -0400

    geo localizacion a home

---
+++ src/helpers/geoLocation.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+export const geoLocation = () => {
+
+  const [geo, setGeo] = useState({})
+  const [selec, setSelec] = useState(false)
+
+  const locationPosicion = () => {
+    const geolocation = navigator.geolocation;
+    geolocation.getCurrentPosition(getPosition, error, options);
+  };
+
+  const options = {
+    enableHightAccuracy: true,
+    timeout: 5000,
+    maximunAge: 0,
+  };
+
+  const getPosition = (position) => {
+    const cordenadas ={
+      lat:  position.coords.latitude, 
+      long: position.coords.longitude,
+    }
+    setGeo(cordenadas)
+    setSelec(true)
+  };
+
+  const error = (error) => {
+    setGeo(error)
+  };
+
+  useEffect(() => {
+    locationPosicion()
+    
+  }, [])
+  
+
+
+  /* console.log(geo, '..dentro') */
+
+  return {geo, selec}
+}
+
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,15 +6,33 @@ import { Sun } from '../components/home/sun/Sun';
 import { Days } from '../components/home/day/Days';
 import { Hours } from '../components/home/hours/Hours';
 import { CityContext } from '../reducer/cityContext';
+import { geoLocation } from '../helpers/geoLocation';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 
 export const Home = () => {
 
+const {geo, selec} = geoLocation()
+const [gl, setGl] = useState('')
+
+
+
+useEffect(() => {
+  selec? setGl(`${geo.lat} ${geo.long}`) : setGl("Tarija")
+}, [selec])
+
+
+console.log(gl, selec)
+
+
   const{citys} = useContext(CityContext)
 
  const buscar = citys.find(c => c.active === true)
- let cuidad = buscar? buscar.city   : 'Tarija'
- /* console.log(cuidad) */
+
+
+ let cuidad = buscar? buscar.city   :gl
+
 
 
   const {cargando, weatherData} = useDataWeather(cuidad)
